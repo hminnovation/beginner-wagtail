4160f62c86674a1ae967b6fb02a7b3e5ddb09e5b
Edd Baldry <edd@potatolondon.com>
2017-01-17 08:49:19 +0000

    Adding ImageChooserPanel to imports on skills models.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/skills/models.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/skills/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailsearch import index
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
